1686dc6984fd5c16ab4509669222446e3ef6d57d
bangvo22 <bang.a.vo@pkh.vn>
2022-11-19 02:16:50 +0700

    feat:    - Login with user role
       [ LOGIN-LOGOUT-ADMIN ]

--- saleapp/admin.py
+++ saleapp/admin.py
@@ -1,37 +1,61 @@
+from flask import redirect
+
 from saleapp import app, db
-from flask_admin import Admin
+from flask_admin import Admin, BaseView, expose
 from flask_admin.contrib.sqla import ModelView
 
-from saleapp.models import Category, Product
+from saleapp.models import Category, Product, UserRole
+from flask_login import current_user, logout_user
 
 admin = Admin(app=app, name="E-commerce Administration", template_mode='bootstrap4')
 
-class ProductView(ModelView):
-    column_display_pk = True #Hiển thị khóa chính sản phẩm
-    can_view_details = True # Display view
-    can_export = True # # Display export
-    column_searchable_list = ['name','description'] # Display search
-    column_filters = ['name','price'] # Display filters
 
-    #Loại bỏ 1 field hiển thị, vd: Không lấy cột image
+class AuthenticatedModelView(ModelView):
+    def is_accessible(self):
+        return current_user.is_authenticated and current_user.user_role.__eq__(UserRole.ADMIN)
+
+
+class ProductView(ModelView):
+    column_display_pk = True  # Hiển thị khóa chính sản phẩm
+    can_view_details = True  # Display view
+    can_export = True  # # Display export
+    column_searchable_list = ['name', 'description']  # Display search
+    column_filters = ['name', 'price']  # Display filters
+
+    # Loại bỏ 1 field hiển thị, vd: Không lấy cột image
     column_exclude_list = ['image']
 
-    #Đổi tên column
+    # Đổi tên column
     column_labels = {
-        'id':'Mã sản phẩm',
+        'id': 'Mã sản phẩm',
         'name': 'Tên sản phẩm',
-        'description':'Mô tả',
-        'price':'Giá bán',
-        'image':'Ảnh đại diện',
-        'category':'Danh mục',
+        'description': 'Mô tả',
+        'price': 'Giá bán',
+        'image': 'Ảnh đại diện',
+        'category': 'Danh mục',
         'active': 'Hoạt động',
-        'created_date':'Ngày tạo'
+        'created_date': 'Ngày tạo'
 
     }
 
-    column_sortable_list = ['id','name','price'] # Sắp xếp
+    column_sortable_list = ['id', 'name', 'price']  # Sắp xếp
 
 
-admin.add_view(ModelView(Category, db.session))
-#admin.add_view(ModelView(Product, db.session))
-admin.add_view(ProductView(Product,db.session)) # Xem chi tiết sản phẩm
+class LogoutView(BaseView):
+    @expose('/')
+    def home_admin_page(self):
+        logout_user()
+        return redirect('/admin')
+
+    def is_accessible(self):
+        return current_user.is_authenticated
+
+
+# admin.add_view(ModelView(Category, db.session))
+admin.add_view(AuthenticatedModelView(Category, db.session))
+
+# admin.add_view(ModelView(Product, db.session))
+#admin.add_view(ProductView(Product, db.session))  # Xem chi tiết sản phẩm
+admin.add_view(AuthenticatedModelView(Product, db.session))
+
+admin.add_view(LogoutView(name="Logout"))
--- saleapp/utils.py
+++ saleapp/utils.py
@@ -4,9 +4,9 @@ import os
 
 from saleapp import app, db
 
-from saleapp.models import Category, Product, User
+from saleapp.models import Category, Product, User, UserRole
 
-import hashlib #băm password
+import hashlib  # băm password
 
 
 # Đọc cho global
@@ -65,7 +65,6 @@ def get_product_by_id(product_id):
     return Product.query.get(product_id)
 
 
-
 # Đếm số lượng sản phẩm với filter
 def count_products():
     # select count * from Product
@@ -73,18 +72,21 @@ def count_products():
 
 
 # create new object
-def add_user(name, username,password, **kwargs):
+def add_user(name, username, password, **kwargs):
     password = str(hashlib.md5(password.strip().encode('utf8')).hexdigest())
 
-    user = User(name=name.strip(),username=username.strip(),password=password,email=kwargs.get('email'), avatar=kwargs.get('avatar'))
+    user = User(name=name.strip(), username=username.strip(), password=password, email=kwargs.get('email'),
+                avatar=kwargs.get('avatar'))
 
     db.session.add(user)
     db.session.commit()
 
-def check_login(username, password):
+
+def check_login(username, password, role=UserRole.USER):
     if username and password:
         password = str(hashlib.md5(password.strip().encode('utf-8')).hexdigest())
-        return User.query.filter(User.username.__eq__(username.strip()), User.password.__eq__(password)).first()
+        return User.query.filter(User.username.__eq__(username.strip()), User.password.__eq__(password),
+                                 User.user_role.__eq__(role)).first()
 
 
 def get_user_by_id(user_id):
